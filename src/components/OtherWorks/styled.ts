b5f58546854096ff98eb1f1e563e9d3e0c7b0ab0
Vicatoriya <vicatoriya2408@gmail.com>
2024-09-26 19:32:04 +0300

    main page without pagination and menu

--- src/components/OtherWorks/styled.ts
+++ src/components/OtherWorks/styled.ts
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+// export const Container = styled.div`
+//   margin-bottom: 50px;
+// `;
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  background-color: #333;
+  color: white;
   margin-bottom: 50px;
+`;
+
+export const A = styled.a`
+  color: white;
+  text-decoration: none;
+  font-size: 18px;
+  margin-left: 5px;
+`;
+
+export const Favorites = styled.div`
+  display: flex;
+  align-item: center;
 `;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { HashRouter, Link, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Home from './pages/Home';
+import { GlobalStyle } from './constants/GlobalStyle';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
-
-
