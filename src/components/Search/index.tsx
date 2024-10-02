c55ad724697454c37728559e3a1ef5e91f29fd09
Vicatoriya <vicatoriya2408@gmail.com>
2024-10-02 23:39:14 +0300

    feat: add form validation

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,21 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import searchImg from '../../assets/search.png';
 import {
   SearchContainer,
   SearchIcon,
   SearchInput,
-  SearchSection,
+  SearchForm,
+  ErrorMessage,
 } from './styled';
 
 export default function SearchBar() {
+  const [query, setQuery] = useState('');
+  const [error, setError] = useState('');
+
+  const validateSearch = (input: string) => {
+    if (input.trim() === '') {
+      setError('Поле не может быть пустым');
+      return false;
+    }
+    if (input.length < 3) {
+      setError('Введите минимум 3 символа');
+      return false;
+    }
+    if (/[^a-zA-Z0-9\s]/.test(input)) {
+      setError('Недопустимые символы в поисковом запросе');
+      return false;
+    }
+    setError('');
+    return true;
+  };
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (validateSearch(query)) {
+    }
+  };
+
   return (
-    <SearchSection>
-      <SearchContainer>
-        <SearchInput placeholder="Search Art, Artist, Work..." />
+    <SearchForm onSubmit={handleSearch}>
+      <SearchContainer error={!!error}>
+        <SearchInput
+          type="text"
+          value={query}
+          placeholder="Search Art, Artist, Work..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
         <SearchIcon>
           <img src={searchImg} alt="Search icon"></img>
         </SearchIcon>
       </SearchContainer>
-    </SearchSection>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
+    </SearchForm>
   );
 }
--- src/components/Search/styled.ts
+++ src/components/Search/styled.ts
@@ -1,17 +1,22 @@
 import styled from 'styled-components';
 
-export const SearchSection = styled.section`
+export const SearchForm = styled.form`
   margin: 25px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5px;
 `;
 
-export const SearchContainer = styled.div`
-  margin: auto;
+export const SearchContainer = styled.div<{ error: boolean }>`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  border: ${({ error }) => (error ? '2px solid red' : 'none')};
   background-color: #f5f5f5;
   border-radius: 25px;
   padding: 10px 20px;
+  width: 80%;
   max-width: 500px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 `;
@@ -41,4 +46,11 @@ export const SearchIcon = styled.button`
     height: 20px;
     color: #555;
   }
+`;
+
+export const ErrorMessage = styled.span`
+  color: red;
+  font-size: 15px;
+
+  text-align: center;
 `;
