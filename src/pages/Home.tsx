93cb2823fd4a50002afad60927a5adb11f1e31a6
Vicatoriya <vicatoriya2408@gmail.com>
2024-10-04 14:47:21 +0300

    fix: aliases

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import SearchBar from '../components/SearchBar';
-import Gallery from '../components/Gallery';
+import { useEffect, useState } from 'react';
+import SearchBar from '@components/SearchBar';
+import Gallery from '@components/Gallery';
 import ImgList from '../components/ImgList';
 import Heading from '../components/StandardHeading';
 import Header from '../components/Header';
@@ -37,7 +37,7 @@ export default function Home() {
   }
 
   function parseImagesJSON(json: any): Array<ImageInformation> {
-    let arr: Array<ImageInformation> = [];
+    const arr: Array<ImageInformation> = [];
     for (let i = 0; i < json.data.length; i++) {
       arr[i] = {
         id: json.data[i].id,
